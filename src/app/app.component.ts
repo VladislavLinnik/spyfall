2344fa4bf773892c649f745c17a38ebbe1438033
Vladislav Linnik <vlad.l@houstonapps.co>
2020-10-13 11:47:21 +0300

    The first version was implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {locations} from './locations';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'spy';
+  playersNumber = 3;
+  isStartScreenComplete = false;
+  isHiddenCard = true;
+  location: string;
+  currentPlayer = {};
+  currentPlayerNumber = 1;
+  isFinishedGame = false;
+  randomSpy = Math.floor(Math.random() * this.playersNumber) + 1;
+
+  start(): void {
+    this.isStartScreenComplete = true;
+    const random = Math.floor(Math.random() * locations.length);
+    this.location = locations[random];
+    this.generatePlayers();
+  }
+
+  nextPlayer(): void {
+    this.isHiddenCard = true;
+    ++this.currentPlayerNumber;
+
+    if (this.currentPlayerNumber === this.playersNumber + 1) {
+      this.isFinishedGame = true;
+    } else {
+      this.generatePlayers();
+    }
+  }
+
+  private generatePlayers(): void {
+    const role = this.randomSpy === this.currentPlayerNumber ? 'Шпион' : 'Местный';
+    this.currentPlayer = Object.assign({}, {
+      number: this.currentPlayerNumber,
+      role
+    });
+  }
 }
